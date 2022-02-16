a8c1ef5b9324e8d6b0130ac5ea65c85cef0d571d
Achyuth Naidu <ashwath.bly@gamil.com>
2022-02-16 14:41:06 +0530

    Added : Helloworld Angular App to display 'Hello from bridgeLabz'

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'NewApp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss',]
+})
+
+/**
+ * The main component of the application.           
+ */
+export class AppComponent {
+  title = 'HelloWorld';
+}
